4e1f54902af7a21876f9ba6e166e0350d3c935fe
roni-ui <ronic1996@gmail.com>
2024-02-17 17:45:20 +0700

    add post index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class PostController extends Controller
 {
@@ -12,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return "hello";
+        $posts = Post::all();
+        return Inertia::render("Post/Index", ["posts"=>$posts]);
     }
 
     /**
---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <ol type="1">
+        <li v-for="post in posts" :key="post.id">
+            <h3>{{post.id}}. {{post.title}}</h3>
+            <p>{{post.description}}</p>
+            <hr>
+        </li>
+    </ol>
+</template>
+
+<script setup>
+defineProps(["posts"]);
+</script>
