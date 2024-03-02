7de3bceed2671735fe20d86990eae29de32b7cff
roni-ui <ronic1996@gmail.com>
2024-03-02 12:35:09 +0700

    add event routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -35,6 +36,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/posts',[PostController::class, 'index']);
+    Route::get('/events',[EventController::class, 'index']);
 });
 
 require __DIR__.'/auth.php';
