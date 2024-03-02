4a83a1ba720fae8f0a8c9d5fb87a8dc1c11f24c3
roni-ui <ronic1996@gmail.com>
2024-03-02 12:33:35 +0700

    add factory event

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title"=> fake()->sentence(1),
+            "description"=> fake()->paragraph(2),
+            "location"=> fake()->sentence(1),
+            "datetime"=> fake()->dateTime(),
+        ];
+    }
+}
