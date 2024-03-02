7f57a4d465ce05e36a4393d3a7e6915fd7a37646
roni-ui <ronic1996@gmail.com>
2024-03-02 12:34:38 +0700

    add event index

---
+++ resources/js/Pages/Event/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <ol type="1">
+        <li v-for="event in events" :key="event.id">
+            <h3>{{event.id}}. {{event.title}}</h3>
+            <p>{{event.description}}</p>
+            <hr>
+            <p>{{event.location}}</p>
+            <hr>
+            <p>{{event.datetime}}</p>
+        </li>
+    </ol>
+</template>
+
+<script setup>
+defineProps(["events"]);
+</script>
